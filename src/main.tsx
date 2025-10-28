46ab4363e7c0e7e8234d0a70798d77326cc3e4fe
Riina Maria Korpela <riina.maria.korpela@gmail.com>
2025-10-28 10:07:55 +0100

    chore: install react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 const rootElement = document.getElementById('root')
 if (!rootElement) {
@@ -10,6 +19,6 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
