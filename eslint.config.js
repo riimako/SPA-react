ae02a0b057c22b00f8fc4ca65146cebafb020118
Riina Maria Korpela <riina.maria.korpela@gmail.com>
2025-10-28 09:42:26 +0100

    chore: install typescript and format with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      tseslint.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
